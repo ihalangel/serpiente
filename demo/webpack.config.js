4b05df0d231378cba61839469ecdaea8b07047d0
snake-dev <snack.dev@gmail.com>
2025-10-12 18:36:21 +0000

    initial project

---
+++ demo/webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path')
+var loadCss = require('webpack-loadcss');
+const port = process.env.PORT || 3000
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'index.js'),
+	output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+	},
+    plugins: [loadCss],
+	module: {
+		rules: [
+			{
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+			},
+			{
+                test: /\.css/,
+                use: ['style-loader', 'css-loader']
+			},
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true,
+        transportMode: 'ws',
+        injectClient: false
+      },
+    devtool: 'inline-source-map'
+}
