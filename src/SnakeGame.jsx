74746327b57b0f0b5434f485d9fb19c20eeb1e2d
ihalangel <ihalangel@gmail.com>
2025-10-15 23:57:45 +0000

    feat: add second snake with independent controls and scoring
    - Initialize two snakes with distinct positions and colors
    - Implement independent movement for each snake in game loop
    - Assign WASD keys to player 1 and arrow keys to player 2
    - Track scores separately and update high score accordingly
    - Keep rendering modular with SnakeBody and Apple components
    
    This commit enables two players to control two snakes independently, enhancing gameplay and laying groundwork for further multiplayer features.

--- src/SnakeGame.jsx
+++ src/SnakeGame.jsx
@@ -19,13 +19,13 @@ class SnakeGame extends React.Component {
       gameLoopTimeout: 50,
       timeoutId: 0,
       startSnakeSize: this.props.startSnakeSize || 6,
-      snakes: [],
+      snakes: [], // Ahora guardamos dos serpientes
       apple: {},
       directionChanged: false,
       isGameOver: false,
-      snakeColor: this.props.snakeColor || this.getRandomColor(),
+      snakeColors: [this.props.snakeColor || this.getRandomColor(), this.getRandomColor()],
       appleColor: this.props.appleColor || this.getRandomColor(),
-      score: 0,
+      score: [0, 0], // Puntajes independientes para cada serpiente
       highScore: Number(localStorage.getItem('snakeHighScore')) || 0,
       newHighScore: false,
     }
@@ -48,22 +48,32 @@ class SnakeGame extends React.Component {
     let blockWidth = width / 30
     let blockHeight = height / 20
 
-    let snake = new Snake(
-      width / 2,
+    // Creación de dos instancias Snake con posiciones y colores distintos
+    let snake1 = new Snake(
+      width / 3,
       height / 2,
       this.state.startSnakeSize,
       blockWidth,
       blockHeight,
-      this.state.snakeColor
+      this.state.snakeColors[0]
+    )
+    let snake2 = new Snake(
+      (width / 3) * 2,
+      height / 2,
+      this.state.startSnakeSize,
+      blockWidth,
+      blockHeight,
+      this.state.snakeColors[1]
     )
 
+    // Generar posición inicial aleatoria de apple que no esté sobre ninguna serpiente
     let appleXpos =
       Math.floor(Math.random() * ((width - blockWidth) / blockWidth + 1)) *
       blockWidth
     let appleYpos =
       Math.floor(Math.random() * ((height - blockHeight) / blockHeight + 1)) *
       blockHeight
-    while (snake.occupiesPosition(appleXpos, appleYpos)) {
+    while (snake1.occupiesPosition(appleXpos, appleYpos) || snake2.occupiesPosition(appleXpos, appleYpos)) {
       appleYpos =
         Math.floor(Math.random() * ((height - blockHeight) / blockHeight + 1)) *
         blockHeight
@@ -74,10 +84,10 @@ class SnakeGame extends React.Component {
       height,
       blockWidth,
       blockHeight,
-      snakes: [snake],
+      snakes: [snake1, snake2],
       apple: { Xpos: appleXpos, Ypos: appleYpos },
       isGameOver: false,
-      score: 0,
+      score: [0, 0],
       newHighScore: false,
       gameLoopTimeout: 50,
     })
@@ -86,7 +96,7 @@ class SnakeGame extends React.Component {
   gameLoop() {
     let timeoutId = setTimeout(() => {
       if (!this.state.isGameOver) {
-        let snakes = this.state.snakes.map((snake) => {
+        let snakes = this.state.snakes.map((snake, index) => {
           snake.move(this.state.width, this.state.height)
           if (snake.isSelfCollision()) {
             this.setState({ isGameOver: true })
@@ -94,45 +104,50 @@ class SnakeGame extends React.Component {
           return snake
         })
 
-        let snakeHead = snakes[0].segments[0]
         let apple = this.state.apple
-        if (snakeHead.Xpos === apple.Xpos && snakeHead.Ypos === apple.Ypos) {
-          snakes[0].grow()
-
-          let newAppleX =
-            Math.floor(Math.random() * ((this.state.width - this.state.blockWidth) / this.state.blockWidth + 1)) *
-            this.state.blockWidth
-          let newAppleY =
-            Math.floor(Math.random() * ((this.state.height - this.state.blockHeight) / this.state.blockHeight + 1)) *
-            this.state.blockHeight
-          while (snakes[0].occupiesPosition(newAppleX, newAppleY)) {
-            newAppleX =
+        // Revisar colisión de manzana con cada serpiente y actualizar puntaje
+        snakes.forEach((snake, idx) => {
+          let head = snake.segments[0]
+          if (head.Xpos === apple.Xpos && head.Ypos === apple.Ypos) {
+            snake.grow()
+            let newAppleX =
               Math.floor(Math.random() * ((this.state.width - this.state.blockWidth) / this.state.blockWidth + 1)) *
               this.state.blockWidth
-            newAppleY =
+            let newAppleY =
               Math.floor(Math.random() * ((this.state.height - this.state.blockHeight) / this.state.blockHeight + 1)) *
               this.state.blockHeight
-          }
-          apple = { Xpos: newAppleX, Ypos: newAppleY }
+            while (snakes.some(s => s.occupiesPosition(newAppleX, newAppleY))) {
+              newAppleX =
+                Math.floor(Math.random() * ((this.state.width - this.state.blockWidth) / this.state.blockWidth + 1)) *
+                this.state.blockWidth
+              newAppleY =
+                Math.floor(Math.random() * ((this.state.height - this.state.blockHeight) / this.state.blockHeight + 1)) *
+                this.state.blockHeight
+            }
+            apple = { Xpos: newAppleX, Ypos: newAppleY }
 
-          let score = this.state.score + 1
-          let highScore = this.state.highScore
-          let newHighScore = this.state.newHighScore
-          let gameLoopTimeout = this.state.gameLoopTimeout
-          if (score > highScore) {
-            highScore = score
-            localStorage.setItem('snakeHighScore', highScore)
-            newHighScore = true
-          }
-          if (gameLoopTimeout > 25) gameLoopTimeout -= 0.5
+            // Actualizar puntaje de la serpiente que comió
+            let newScores = [...this.state.score]
+            newScores[idx] += 1
 
-          this.setState({
-            score,
-            highScore,
-            newHighScore,
-            gameLoopTimeout,
-          })
-        }
+            let highScore = this.state.highScore
+            let newHighScore = this.state.newHighScore
+            let gameLoopTimeout = this.state.gameLoopTimeout
+            if (newScores[idx] > highScore) {
+              highScore = newScores[idx]
+              localStorage.setItem('snakeHighScore', highScore)
+              newHighScore = true
+            }
+            if (gameLoopTimeout > 25) gameLoopTimeout -= 0.5
+
+            this.setState({
+              score: newScores,
+              highScore,
+              newHighScore,
+              gameLoopTimeout,
+            })
+          }
+        })
 
         this.setState({
           snakes,
@@ -169,33 +184,52 @@ class SnakeGame extends React.Component {
     }
     if (this.state.directionChanged) return
 
-    let snake = this.state.snakes[0]
+    // Control para la serpiente 1 (flechas)
+    let snake1 = this.state.snakes[0]
+    // Control para la serpiente 2 (WASD)
+    let snake2 = this.state.snakes[1]
+
     switch (event.keyCode) {
-      case 37:
-      case 65:
-        snake.changeDirection('left')
+      // Serpiente 1
+      case 37: // left arrow
+        snake1.changeDirection('left')
         break
-      case 38:
-      case 87:
-        snake.changeDirection('up')
+      case 38: // up arrow
+        snake1.changeDirection('up')
         break
-      case 39:
-      case 68:
-        snake.changeDirection('right')
+      case 39: // right arrow
+        snake1.changeDirection('right')
         break
-      case 40:
-      case 83:
-        snake.changeDirection('down')
+      case 40: // down arrow
+        snake1.changeDirection('down')
         break
+
+      // Serpiente 2
+      case 65: // A
+        snake2.changeDirection('left')
+        break
+      case 87: // W
+        snake2.changeDirection('up')
+        break
+      case 68: // D
+        snake2.changeDirection('right')
+        break
+      case 83: // S
+        snake2.changeDirection('down')
+        break
+
       default:
-        break
+        return
     }
 
-    this.setState({ directionChanged: true, snakes: [snake] })
+    this.setState({
+      directionChanged: true,
+      snakes: [snake1, snake2],
+    })
   }
 
   render() {
-    if (!this.state) return null  // Protección para evitar error por state null
+    if (!this.state) return null
 
     if (this.state.isGameOver) {
       return (
@@ -204,7 +238,7 @@ class SnakeGame extends React.Component {
           height={this.state.height}
           highScore={this.state.highScore}
           newHighScore={this.state.newHighScore}
-          score={this.state.score}
+          score={this.state.score.reduce((a, b) => a + b, 0)}
         />
       )
     }
@@ -237,8 +271,7 @@ class SnakeGame extends React.Component {
         />
 
         <div id='Score' style={{ fontSize: this.state.width / 20 }}>
-          HIGH-SCORE: {this.state.highScore}&ensp;&ensp;&ensp;&ensp;SCORE:{' '}
-          {this.state.score}
+          HIGH-SCORE: {this.state.highScore} &ensp;&ensp;&ensp;&ensp; SCORE: {this.state.score.reduce((a, b) => a + b, 0)}
         </div>
       </div>
     )
